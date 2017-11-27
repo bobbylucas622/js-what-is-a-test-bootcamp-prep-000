e982875f625123ad95298d2d9a75f89e054f10ca
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-27 05:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 / write your code below!
 var name = "Susan"
 var height = 74
-var message = `${name} is `${height} inches tall
+var message = `${name} is `${height} inches tall` {
+  
+  
+};
 
 
 
